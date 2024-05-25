a352f188a4e1a4c6f08bd4fbba9a141edc15da55
Faran Ali <faranaliahmed@gmail.com>
2024-05-25 14:35:06 +0500

    Add Generator file

---
+++ src/Generators/Generator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Lucid\Generators;
+
+abstract class Generator
+{
+    protected function createDirectory($path): void
+    {
+        if(!is_dir($path)) {
+            mkdir($path);
+        }
+    }
+
+    protected function createRecursiveDirectories(string $root, array $elements): string
+    {
+        $path = $root;
+        foreach ($elements as $element) {
+            $path = $path . DIRECTORY_SEPARATOR . $element;
+            $this->createDirectory($path);
+        }
+        return $path;
+    }
+
+    protected function createFile(string $path, string $contents): void
+    {
+        file_put_contents($path, $contents);
+    }
+}
